f03feb553b2de8577343879e85b7a380ffb7f8e1
cicekozkan <cicekozkan@gmail.com>
2015-07-04 23:37:21 +0300

    // when using default arguments you cannot leave an argument without a default value to the right of a default value argument

---
+++ CppExamples/CppExamples/example45.cpp
@@ -0,0 +1,16 @@
+// when using default arguments you cannot leave an argument without a default value to the right of a default value argument
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//void func(int = 10, int, int); gecersiz
+//void func(int, int = 99, int); gecersiz
+//void func(int, int = 99, int = 23); gecerli
+
+int main()
+{
+
+
+	return 0;
+}
