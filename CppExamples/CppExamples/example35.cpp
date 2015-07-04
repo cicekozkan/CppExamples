a446b706c3c169b54850d6569194dd0c15d57021
cicekozkan <cicekozkan@gmail.com>
2015-07-04 22:13:29 +0300

    example35.cpp -> int (*p1)[10] = pointer to an array of 10 elements

---
+++ CppExamples/CppExamples/example35.cpp
@@ -0,0 +1,15 @@
+// int (*p1)[10] = pointer to an array of 10 elements
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a[10] = { 0 };
+
+	int *p1 = a;
+	int (*p2)[10] = &a;
+	
+	return 0;
+}
