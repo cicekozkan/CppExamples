bc8d74a8fd6409ce8dc231d9cbef5f72d3d81bda
cicekozkan <cicekozkan@gmail.com>
2015-07-05 00:47:21 +0300

    example51 -> const overloading with pointers

---
+++ CppExamples/CppExamples/example51.cpp
@@ -0,0 +1,16 @@
+// const overloading
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void func(int *);
+void func(const int *);
+//islev yuklemesi - const overloading
+
+
+int main()
+{
+
+	return 0;
+}
