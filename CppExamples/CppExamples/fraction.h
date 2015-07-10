35fbcba8d226478a45f51a72d8eb978029bfd21d
cicekozkan <cicekozkan@gmail.com>
2015-07-11 00:07:07 +0300

    fraction >> and << operators overloaded

--- CppExamples/CppExamples/fraction.h
+++ CppExamples/CppExamples/fraction.h
@@ -4,11 +4,12 @@
 //odev sorusu
 
 class Fraction {
-	///
+	int m_nom, m_denom;
 	Fraction &simplify();
 public:
-	Fraction(int nom = 0, int demom = 1);
-
+	Fraction(int nom = 0, int denom = 1) : m_nom{ nom }, m_denom{ denom } {}	// constructor
+	// you don't need to create a copy constructor. default copy constructor will do the job
+	// no need for destructor too. you don't use any resources
 	friend bool operator<(const Fraction &r1, const Fraction &r2);
 	friend bool operator<=(const Fraction &r1, const Fraction &r2);
 	friend bool operator>(const Fraction &r1, const Fraction &r2);
@@ -37,7 +38,6 @@ public:
 	int nominator()const;
 	int denominator()const;
 	operator double()const;
-
 
 	friend std::ostream &operator<<(std::ostream &, const Fraction &);
 	friend std::istream &operator>>(std::istream &, Fraction &);
