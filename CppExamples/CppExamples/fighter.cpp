f6abe3c03e063bbfcb12ff1059eeea3898aa0127
cicekozkan <cicekozkan@gmail.com>
2015-07-10 22:58:22 +0300

    write comments in fighter header and implementation file

--- CppExamples/CppExamples/fighter.cpp
+++ CppExamples/CppExamples/fighter.cpp
@@ -1,22 +1,21 @@
 #include "fighter.h"
 #include <iostream>
 
-
 using namespace std;
 
-int Fighter::ms_age_limit = 20;
-list<Fighter *> Fighter::ms_list;
+int Fighter::ms_age_limit = 20;		// do not write static in implemetation
+list<Fighter *> Fighter::ms_list;	// do not write static in implemetation
 
-void Fighter::getHelp()
+void Fighter::getHelp()	// implement the member function
 {
-	for (auto ptr : ms_list) {
-		if (ptr != this && ptr->getAge() > ms_age_limit)
-			cout << ptr->getName() << " ";
+	for (auto ptr : ms_list) {	// range based for loop with auto keyword
+		if (ptr != this && ptr->getAge() > ms_age_limit)	// inside a member function implementation you do not need
+			cout << ptr->getName() << " ";					// to use resolution operator :: to use static variable
 	}
 }
 
-Fighter::~Fighter()
-{
+Fighter::~Fighter()	// your class uses a resource: a list with class pointer. you have to implement a destructor
+{					// and free the resources
 	ms_list.remove(this);
 }
 
