2afaa27315bfd0fbbb18a58ad10fec85b3b16300
ozkan.cicek <ozkan.cicek@analog.com>
2015-08-07 18:51:32 +0300

    Date: A bug in operator< fixed

--- CppExamples/CppExamples/date.cpp
+++ CppExamples/CppExamples/date.cpp
@@ -5,11 +5,26 @@ using namespace std;
 
 bool operator<(const Date &r1, const Date &r2)
 {
-	if (r1.getYear() < r2.getYear())	return true;
-	else if (r1.getMonth() < r2.getMonth())	return true;
-	else if (r1.getMonthDay() < r2.getMonthDay()) return true;
-	else return false;
+	if (r1.getYear() < r2.getYear())
+	{
+		return true;
+	}
+	else if (r1.getYear() == r2.getYear()){
+		if (r1.getMonth() < r2.getMonth())
+		{
+			return true;
+		}
+		else if (r1.getMonth() == r2.getMonth())
+		{
+			if (r1.getMonthDay() < r2.getMonthDay())
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
+
 
 ostream& operator<<(ostream& os, const Date& d)
 {
@@ -18,11 +33,12 @@ ostream& operator<<(ostream& os, const Date& d)
 
 int main()
 {
-	Date d1;
-	Date d2(6, 11, 1987);
+	Date d1(7, 11, 1988);
+	Date d2(7, 11, 1987);
 
 	cout << d1 << endl;
 	cout << d2 << endl;
+	cout << (d1 < d2) << endl;
 
 	getchar();
 
