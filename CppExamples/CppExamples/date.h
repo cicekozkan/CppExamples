4373d2cb8904c2dccd188fe180662dc2071392b5
cicekozkan <cicekozkan@gmail.com>
2015-08-22 16:02:26 +0300

    Date -= is working but does not take leap year day into account

--- CppExamples/CppExamples/date.h
+++ CppExamples/CppExamples/date.h
@@ -5,9 +5,11 @@
 class Date {
 	int m_total_days;	
 	int m_day, m_mon, m_year;
-	static const int monthDays[12];
+	static const int monthDays[12]; 
 	static const int mscYearBase = 1700;
-	static bool isleap(int y);	// modulo 4 years
+	static bool isleap(int y){// modulo 4 years
+		return (y % 4 == 0);
+	}	
 public:
 	Date(int d = 0, int m = 0, int y = 0){
 		if (d == 0 || m == 0 || y == 0){
@@ -92,13 +94,18 @@ public:
 	Date &operator-=(int ndays){
 		m_day -= ndays;
 		while (m_day <= 0){
-			m_day += monthDays[m_mon - 2];
 			--m_mon;
 			if (m_mon <= 0) {
 				--m_year;
 				m_mon += 12;
 			}
-			if (m_year < mscYearBase)	break;
+			if (m_year < mscYearBase)	{
+				m_day = 1;
+				m_mon = 1;
+				m_year = mscYearBase;
+				return *this;
+			}
+			m_day += monthDays[m_mon - 1];			
 		}
 		return *this;
 	}
