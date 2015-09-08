af70e755940c06e375edf96c0526accfd036467f
cicekozkan <cicekozkan@gmail.com>
2015-09-09 00:19:29 +0300

    Example5_090506.cpp -> if the function template takes its arguments by reference array to pointer conversion does not occur

---
+++ CppExamples/CppExamples/09_05_06/Example5_090506.cpp
@@ -0,0 +1,19 @@
+// if the function template takes its arguments by reference array to pointer conversion does not occur
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+void func(T &x)
+{
+	cout << typeid(T).name() << endl; //int [10]
+}
+
+int main()
+{
+	int a[10] = { 0 };	
+
+	func(a);
+
+	return 0;
+}
